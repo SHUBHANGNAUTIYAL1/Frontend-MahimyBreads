1b9c5fa25e127f992fb147ede185e6e10f680048
Shubhang <shubhang999454@gmail.com>
2024-05-21 20:07:06 +0530

    added a home navigation link

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -24,9 +24,9 @@ export const Navbar2 = () => {
         <img src={logo} alt="logo" className="w-[100%]" width={100} />
         
       </a>
-      <div className="hidden md:flex gap-16 text-white  justify-between items-center font-arhivo">
+      <div className="hidden md:flex gap-12 text-white  justify-between items-center font-arhivo">
         
-       
+      <Link to="/" className="hover:text-gray-300">Home</Link>
         <Link to="/about" className="hover:text-gray-300">About us</Link>
         <Link to="/book" className="hover:text-gray-300">Where to Buy</Link>
         <button onClick={handleSubmit} className="bg-[#A4CF25] text-black p-2 md:px-4 rounded-lg w-[137px] h-[46px]">
@@ -49,6 +49,7 @@ export const Navbar2 = () => {
       {showMenu && (
         <div className="  md:hidden fixed top-0 left-0 w-[85vw] h-[100%]  backdrop-blur-[0.8rem] flex flex-col gap-10 py-10 items-end pr-12  bg-gradient-to-l from-[#230c02] to-[#221609] text-white z-[50] font-semibold text-[24px] ">
           <X className=" text-2xl cursor-pointer " onClick={toggleMenu} />
+          <Link to="/" onClick={toggleMenu} className="hover:text-gray-300">Home</Link>
           <Link to="/about" onClick={toggleMenu} className="hover:text-gray-300">About Us</Link>
           <Link to="/book" onClick={toggleMenu} className="hover:text-gray-300">Where to Buy</Link>
          
